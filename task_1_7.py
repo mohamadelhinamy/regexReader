824d02b33d186f1d21040f573782bea6abcf0369
mohamadelhinamy <mohmadelhinamy7@gmail.com>
2019-03-03 21:42:09 +0200

    luv ya

---
+++ task_1_7.py
@@ -0,0 +1,26 @@
+import argparse
+import re
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(add_help=True, description='Sample Commandline')
+
+    parser.add_argument('--file', action="store", help="path of file to take as input", nargs="?",
+                        metavar="file")
+
+    args = parser.parse_args()
+
+    print(args.file)
+    
+    f = open(args.file, "r")
+    text = f.read()
+    pattern=re.finditer("[^(A-Z)*]|[$(0-9)*]",text)
+    pattern = [m.group(0) for m in pattern]
+    
+    f = open("task_1_7_result.txt", "w")
+    for m in pattern: 
+      print(m)
+      f.write(m)
+      f.write("\n")
+      
+    f.close()
+    print(pattern)
